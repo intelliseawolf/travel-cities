2c317f0128c74079d363d2307112e1de701073b7
intellieseawolf <intelliseawolf103@gmail.com>
2023-03-08 15:54:23 -0500

    added: min width of the passenger

--- src/components/DateInput/components.tsx
+++ src/components/DateInput/components.tsx
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 
 export const StyledDatePicker = styled(DatePicker)`
   width: 100%;
-  min-width: 92px;
+  min-width: 100px;
   font-size: 1rem;
   padding: 0.375rem 0.375rem;
   border: 1px solid #e5e7eb;
--- src/components/PassengerInput/components.tsx
+++ src/components/PassengerInput/components.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const InputWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  min-width: 92px;
+  min-width: 100px;
   border: 1px solid #e5e7eb;
   border-radius: 6px;
   padding: 6px 10px;
