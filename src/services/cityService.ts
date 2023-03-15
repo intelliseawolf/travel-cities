c457bb6a4fe25166b15b738cb104a75af0991c4e
intellieseawolf <intelliseawolf103@gmail.com>
2023-03-15 00:43:43 -0400

    fixed: env file

--- src/services/cityService.ts
+++ src/services/cityService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const serverURL = process.env.SERVER_URL || "https://localhost:3000/api";
+const serverURL =
+  process.env.REACT_APP_SERVER_URL || "https://localhost:3000/api";
 
 const searchCity = (keyword: string) => {
   return axios.get(`${serverURL}/search_cities`, { params: { keyword } });
